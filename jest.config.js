8e96e204ad13c46194ee0542da81811838835a0c
aiden01 <liangu@protonmail.com>
2019-08-31 11:57:46 -0400

    remove unnecessary stuff

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "typescript-starter",
-  preset: "ts-jest",
-  automock: false,
-  collectCoverage: true,
-  reporters: ["default", "jest-junit"],
-  testPathIgnorePatterns: ["dist/.*"],
-}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-require("dotenv").config()
-import { logger } from "./logger"
-
-export const greet = (thing: string) => `Hello ${thing}`
-
-const main = async () => {
-  logger.info(greet("World"))
-}
-
-main()
+console.log("Hello, world")
